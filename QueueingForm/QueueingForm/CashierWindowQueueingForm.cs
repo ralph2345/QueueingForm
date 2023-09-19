717d5355c35062ccfbcbc2642b4857c6d1741d58
ralph2345 <144690960+ralph2345@users.noreply.github.com>
2023-09-19 11:03:17 +0800

    Add files via upload

---
+++ QueueingForm/QueueingForm/CashierWindowQueueingForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QueueingForm
+{
+    public partial class CashierWindowQueueingForm : Form
+    {
+        public CashierWindowQueueingForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            DisplayCashierQueue(CashierClass.CashierQueue);
+        }
+
+        public void DisplayCashierQueue(IEnumerable CashierList)
+        {
+            listCashierQueue.Items.Clear();
+            foreach (Object obj in CashierList)
+            {
+                listCashierQueue.Items.Add(obj.ToString());
+            }
+        }
+
+        private void CashierWindowQueueingForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
